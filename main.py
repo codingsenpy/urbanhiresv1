70bfc3fa4260205e6b657c07df6f980bd3355ad5
codingsenpy <mustafakunain06@gmail.com>
2024-08-10 14:04:20 +0530

    additional pages

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for,jsonify
+from flask import Flask, render_template, request, jsonify, session, redirect, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+import os
 
 '''
 Red underlines? Install the required packages first: 
@@ -14,52 +17,87 @@ This will install the packages from requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-
-joblist=[{
-    "jobtype":"defualt",
-"pay":"nomoney",
-    "location":"earth",
-    "time":"anytime"
-}]
-accepted_jobs_data = []
-
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+mongo = PyMongo(app)
+db = mongo.db
+app.secret_key = os.urandom(24)
 @app.route('/')
 def home():
     return render_template("index.html")
 
-
-@app.route("/hire",methods=["GET","POST"])
+@app.route("/hire", methods=["GET", "POST"])
 def hire():
-    if(request.method=="POST"):
-        newjob={
-            "jobtype": request.form["jobtype"],
-            "pay":request.form["pay"],
-            "location":request.form["location"],
-            "time":request.form["time"]
-        }
-        joblist.append(newjob)
-        return 'success'
+    if request.method == "POST":
+        # Generate or retrieve a session ID for the user
+        if 'user_id' not in session:
+            session['user_id'] = str(ObjectId())
 
-    return render_template("hire.html")
-@app.route("/work",methods=['POST' , 'GET'])
+        new_job = {
+            "jobtype": request.form["jobtype"],
+            "pay": request.form["pay"],
+            "location": request.form["location"],
+            "time": request.form["time"],
+            "user_id": session['user_id']
+        }
+        db.jobs.insert_one(new_job)
+        return redirect(url_for('my_jobs'))  # Redirect to a new page to show posted jobs
+
+    return render_template("hire.html")             # Render hire.html for GET request
+
+@app.route("/my_jobs", methods=["GET"])
+def my_jobs():
+    if 'user_id' in session:
+        user_jobs = list(db.jobs.find({"user_id": session['user_id']}))
+        return render_template("my_jobs.html", jobs=user_jobs)
+    return "No jobs found for this user."
+
+
+@app.route("/work", methods=['POST', 'GET'])
 def work():
-    return render_template("work.html",jobs=joblist)
+    jobs = list(db.jobs.find())
+    print(jobs)
+    return render_template("work.html", jobs=jobs)
 
 @app.route("/accept_job", methods=['POST'])
 def accept_job():
     job_data = request.get_json()
-    job_index = job_data['job_index']
-    accepted_job = joblist[job_index]
-    accepted_jobs_data.append(accepted_job)
-    return jsonify({'status': 'success'})
+    job_id = job_data["job_id"]
+    job = db.jobs.find_one({"_id": ObjectId(job_id)})
+
+    if job:
+        db.accepted_jobs.insert_one(job)
+        db.jobs.delete_one({"_id": ObjectId(job_id)})
+        return jsonify({'status': 'success'})
+    return jsonify({'status': 'error', 'message': 'Job not found'}), 404
+
 @app.route("/accepted_jobs", methods=['GET'])
 def show_accepted_jobs():
-    return render_template("accepted_jobs.html", accepted_jobs=accepted_jobs_data)
+    accepted_jobs = list(db.accepted_jobs.find())
+    print(accepted_jobs) 
+    return render_template("accepted_jobs.html", accepted_jobs=accepted_jobs)
+
+@app.route("/delete_job/<job_id>", methods=["DELETE"])
+def delete_job(job_id):
+    result = db.jobs.delete_one({"_id": ObjectId(job_id)})
+    if result.deleted_count > 0:
+        return jsonify({'status': 'success'})
+    else:
+        return jsonify({'status': 'error', 'message': 'Job not found'}), 404
+
+@app.route("/edit_job/<job_id>", methods=["GET", "POST"])
+def edit_job(job_id):
+    if request.method == "POST":
+        updated_job = {
+            "jobtype": request.form["jobtype"],
+            "pay": request.form["pay"],
+            "location": request.form["location"],
+            "time": request.form["time"]
+        }
+        db.jobs.update_one({"_id": ObjectId(job_id)}, {"$set": updated_job})
+        return redirect(url_for('my_jobs'))
+
+    job = db.jobs.find_one({"_id": ObjectId(job_id)})
+    return render_template("edit_job.html", job=job)
+
 if __name__ == "__main__":
     app.run(debug=True)
-# 	<ul>
-#         {{ jobs }}
-# <!--        {% for job in jobs %}-->
-# <!--        <li>{{ job['jobtype'] }} - {{job.pay}} - {{job.location}} - {{job.time}}</li>-->
-# <!--        {% endfor %}-->
-#     </ul>
